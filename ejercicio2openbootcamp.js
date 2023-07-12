5cf8b4d4e29c3b273dda31c36e820ead01d9ef37
Esteban <estebanvelascoriano@gmail.com>
2023-07-12 15:05:04 -0500

    2 ejercicio

---
+++ ejercicio2openbootcamp.js
@@ -0,0 +1,9 @@
+const cosas_sobre_mi = [ 
+     desarrollador= {
+        nombre:"esteban velasco riano",
+        edad: 20, 
+        soydesarrollador: undefined,
+        fechadenacimiento: fechade_nacimiento = new Date(2003,0,20)
+        }
+ ]
+ console.log(cosas_sobre_mi)
